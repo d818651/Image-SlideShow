b00839a990c4797b8cf7aa7f012c1dd01f34d0af
Bilous <d818651@gmail.com>
2018-11-08 00:35:16 +0200

    rewrite function's

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
-var time = 5;
 var timerId;
 var currentImg = 0;
 var imgSrs;
 
-function getSrs(){
-  var inputList = document.getElementById('inputList');
+function getSrs() {
+  var inputList = document.getElementById("inputList");
   var text = inputList.value;
   imgSrs = text.split("\n");
-  return imgSrs.length-1;
+  return imgSrs.length - 1; //возвращаю количество строк =-1
 }
 
 function run() {
-  debugger;
-  if (getDelay() == -1 || getSrs()==-1 ) return;
-  document.querySelector("h2").innerHTML = "<img id=\"img\" alt=\"Image text\">"; // создаю новый элемент картинка
-  timerId = setInterval(changeImg, getDelay());  
-  changeImg();
+  if (getDelay() == -1 || getSrs() == -1) return;
+  document.querySelector("h2").innerHTML = '<img id="img" alt="Image text">'; // создаю новый элемент картинка
+  timerId = setInterval(changeImg, getDelay()); // запускаю таймер
+  changeImg(); // чтобы моментально обновить картинку
   // inputList.hidden = true; // скрываю текст от изменения
 }
 
 function changeImg() {
-   {
     var img = document.getElementById("img");
     img.src = imgSrs[currentImg];
-    console.log(imgSrs[currentImg]);
-    currentImg++;
+    currentImg++;                                                     // тут пока некрасиво
     if (currentImg >= imgSrs.length - 1) currentImg = 0;
-  }
-
 }
 
 function getDelay() {
-  if (!isNaN(val)) return -1;
   var val = document.getElementById("time").value;
-    time = +val;
-    return time * 1000;
+  if (isNaN(val)) return -1;
+  var time = +document.getElementById("time").value;
+  return time * 1000;
+}
+
+function updateDelay(){
+  clearInterval(timerId);
+  run();
 }
