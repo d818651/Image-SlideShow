cf30e5a01475c26c92439e09d34c7365128764e4
Bilous <d818651@gmail.com>
2018-11-08 17:09:36 +0200

    alfa.05

--- script.js
+++ script.js
@@ -6,40 +6,45 @@ function getSrs() {
   var inputList = document.getElementById("inputList");
   var text = inputList.value + "\n";
   imgSrs = text.split("\n");
-  currentImg = 0;
-  return imgSrs.length - 1; //возвращаю количество строк =-1
+    return imgSrs.length - 1; //возвращаю количество строк =-1
 }
 
 function run() {
-
+  debugger;
+if (timerId>0){
+  clearInterval(timerId);
+  document.getElementById("button").value="Run!";
+  timerId=0;
+} else {
   if (getDelay() == -1 || getSrs() == -1) return;
+  
   document.querySelector("h2").innerHTML = '<img class="img" id="img" alt="">'; // создаю новый элемент картинка
   timerId = setInterval(changeImg, getDelay()); // запускаю таймер
   changeImg(); // вызов чтобы моментально обновить картинку
-  // inputList.hidden = true; // скрываю текст от изменения
+  document.getElementById("button").value="Stop!";
+}// inputList.hidden = true; // скрываю текст от изменения
 }
 
 
 function checkUrl(id) {
-
+//debugger;
   imgSrs[id] = imgSrs[id].trim();
   var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi
   var regexp = new RegExp(expression);
-  if (regexp.test(imgSrs[id]) == true && imgSrs[id].indexOf(" ") == -1) return imgSrs[id];
-  else {
+  if (regexp.test(imgSrs[id]) == true && imgSrs[id].indexOf(" ") == -1) {
+    return imgSrs[id];
+  } else {
     clearInterval(timerId);
     return false;
   }
 }
 
 function changeImg() {
-  debugger;
   var img = document.getElementById("img");
   if (currentImg >= imgSrs.length - 1) currentImg = 0;
   img.src = checkUrl(currentImg);
-  img.alt = "Image №"+(currentImg+1)+" " + imgSrs[currentImg].substring(imgSrs[currentImg].lastIndexOf("/")+1 ) + " has wrong URL";
+  img.alt = "Image №" + (currentImg + 1) + " " + imgSrs[currentImg].substring(imgSrs[currentImg].lastIndexOf("/") + 1) + " has wrong URL";
   currentImg++; // тут пока некрасиво
-
 }
 
 function getDelay() {
@@ -52,4 +57,10 @@ function getDelay() {
 function updateDelay() {
   clearInterval(timerId);
   if (timerId != null) run();
+}
+
+function setSize(id, sizeX) {
+  //debugger;
+  document.getElementById(id).style.height = sizeX + "px";
+  //  document.getElementById(id).style.width=sizeY+"%";
 }
